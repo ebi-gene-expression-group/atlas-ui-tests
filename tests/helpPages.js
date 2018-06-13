6327944cc7418e8f259adb638041d54b20cd7939
Monica Jianu <monica@ebi.ac.uk>
2018-06-13 10:27:21 +0100

    Added skeleton test suite for help pages to trigger first Travis CI build.

---
+++ tests/helpPages.js
@@ -0,0 +1,11 @@
+const selenium = require('selenium-webdriver');
+
+beforeAll(done => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
+  done()
+})
+
+describe('Help pages', function () {
+  // TODO
+});
