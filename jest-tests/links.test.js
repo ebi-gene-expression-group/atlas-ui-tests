a2d7f2e79b9de052379d4d45a810d64596e824d9
Monica Jianu <monica@ebi.ac.uk>
2018-09-28 16:33:16 +0100

    Refacored code to extract common utility functions. Added test case for links in the help pages. Added code to load the experiments table and choose a random experiment page URL.

--- jest-tests/links.test.js
+++ jest-tests/links.test.js
@@ -2,6 +2,26 @@ import {Builder, By, until} from 'selenium-webdriver'
 import chrome from 'selenium-webdriver/chrome'
 import fetch from 'node-fetch'
 
+const getUrlsFromHtmlElements = async (htmlElements) => {
+  let queryURLs = []
+
+  for(let element of htmlElements) {
+    let url = await element.getAttribute('href')
+    queryURLs.push(url)
+  }
+
+  return queryURLs
+}
+
+const testUrlResponseCodeIsOk = async (urls) => {
+  for(let url of urls) {
+    console.log(`Fetching url ${url}`)
+    const response = await fetch(url)
+    console.log(response)
+    expect(response.ok).toBe(true)
+  }
+}
+
 const getWebdriver = () => {
   return new Builder()
     .forBrowser("chrome")
@@ -30,23 +50,62 @@ describe('Home page links', () => {
     await driver.findElement(By.id('data-protection-agree')).click()
   });
 
-  it('All example queries should return HTTP status 200', async () => {
-    let form = await driver.findElement(By.id('home-search-atlas-form'))
+  it('All example queries return HTTP status 200', async () => {
+    let geneExamples = await driver.findElement(By.id('home-search-atlas-form')).findElements(By.css('a'))
 
-    let geneExamples = await form.findElements(By.css('a'))
+    let queryURLs = await getUrlsFromHtmlElements(geneExamples)
 
-    let queryURLs = []
+    await testUrlResponseCodeIsOk(queryURLs)
+  })
+})
 
-    for(let gene of geneExamples) {
-      let url = await gene.getAttribute('href')
-      queryURLs.push(url)
-    }
+describe('Help page links', () => {
+  beforeEach(async () => {
+    await driver.get('https://www-test.ebi.ac.uk/gxa/help/index.html')
+    await driver.findElement(By.id('data-protection-agree')).click()
+  });
 
-    for(let url of queryURLs) {
-      console.log(`Fetching url ${url}`)
-      const response = await fetch(url)
-      expect(response.ok).toBe(true)
-    }
+  it('All More help internal links on the right side of the page are valid', async () => {
+    let internalLinks = await driver.findElement(By.id('main-content-area')).findElement(By.css('ul')).findElements(By.css('a'))
+
+    let queryUrls = await getUrlsFromHtmlElements(internalLinks)
+
+    await testUrlResponseCodeIsOk(queryUrls)
+  })
+
+  it('All links within the help page text are valid', async () => {
+    let links = await driver.findElement(By.id('main-content-area')).findElement(By.className('columns')).findElements(By.css('a'))
+
+    let queryUrls = await getUrlsFromHtmlElements(links)
+
+    // TODO Should we be testing external links such as GitHub repos, Reactome, Bioconductor
+    console.log(queryUrls)
+    // await testUrlResponseCode(queryUrls)
+  })
+})
+
+describe('Experiment page links', () => {
+  let randomExperimentUrl
+
+  beforeAll( async () => {
+    await driver.get('https://www-test.ebi.ac.uk/gxa/experiments')
+    await driver.findElement(By.name('experiments-table_length')).sendKeys('All')
+
+    console.log('Wait for experiments table to load')
+    await driver.sleep(5000)
+
+    console.log('Retrieve experiment URLs')
+    let links = await driver.findElements(By.css("a[title='View in Expression Atlas'"))
+
+    let experimentUrls = await getUrlsFromHtmlElements(links)
+
+    randomExperimentUrl = experimentUrls[Math.floor(Math.random()*experimentUrls.length)];
+  })
+
+  it('checks experiment page tabs load correctly', async () => {
+    console.log(`Navigate to experiment page ${randomExperimentUrl}`)
+
+    await driver.get(randomExperimentUrl)
 
   })
 })
